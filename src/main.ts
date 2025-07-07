4c4cdc20c01a45383b0121fdda4ad5641bb81824
Robotz213 <nicholas@robotz.dev>
2025-07-07 17:57:02 -0400

    refactor: remove HelloWorld component and related assets
    chore: delete base.css and main.css, add SCSS files for styling
    
    feat: add Bootstrap logo and integrate BootstrapVue for UI components
    
    fix: update router to redirect to login view and remove unused routes
    
    style: simplify HomeView and create LoginView with form
    
    delete: remove unused components and tests related to the old structure
    
    refactor: update main.ts to use SCSS and BootstrapVue
    
    chore: clean up assets and types definitions

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,14 @@
-import "./assets/main.css";
-
-import { createApp } from "vue";
+import "@/assets/scss/main.css";
+import { createBootstrap } from "bootstrap-vue-next";
+import "bootstrap-vue-next/dist/bootstrap-vue-next.css";
+import "bootstrap/dist/css/bootstrap.css";
 import { createPinia } from "pinia";
-
+import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
-
+app.use(createBootstrap()); // Important
 app.mount("#app");
--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,23 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
-import vueDevTools from 'vite-plugin-vue-devtools'
-
+import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
+import { BootstrapVueNextResolver } from "bootstrap-vue-next";
+import { fileURLToPath, URL } from "node:url";
+import Components from "unplugin-vue-components/vite";
+import { defineConfig } from "vite";
+import vueDevTools from "vite-plugin-vue-devtools";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
     vueDevTools(),
+    Components({
+      resolvers: [BootstrapVueNextResolver()],
+    }),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-})
+});
