c57a7e363ca64649598ff6686f0643d9f63cce19
Robotz213 <nicholas@robotz.dev>
2025-07-08 19:06:30 -0400

    feat: enhance NavbarView with menu icon and text; add styling for menu-heading, nav-link, and nav-link-icon; update main.css for consistent design

--- src/components/NavbarView.vue
+++ src/components/NavbarView.vue
@@ -1,18 +1,22 @@
 <script setup lang="ts">
 import logoCrawjud from "@/assets/img/crawjud.png";
+import MaterialSymbolsMenuRounded from "~icons/material-symbols/menu-rounded?width=24px&height=24px";
 </script>
 
 <template>
   <nav class="navbar navbar-expand-lg bg-body-tertiary rounded-bottom fixed-top">
     <div class="container-fluid">
       <a
-        class="navbar-brand"
+        class="navbar-brand d-flex justify-content-center nav-link"
         data-bs-toggle="offcanvas"
         href="#offCanvasCrawJUD"
         role="button"
         aria-controls="offCanvasCrawJUD"
       >
-        Menu
+        <span class="nav-link-icon">
+          <MaterialSymbolsMenuRounded />
+        </span>
+        <span class="text"> Menu </span>
       </a>
       <div class="me-auto ms-auto d-flex gap-2">
         <img :src="logoCrawjud" alt="Logo CrawJUD" height="35" />
@@ -21,3 +25,35 @@ import logoCrawjud from "@/assets/img/crawjud.png";
     </div>
   </nav>
 </template>
+
+<style lang="css" scoped>
+.menu-heading {
+  padding: 1.4rem 1rem 0.75rem;
+  font-size: 0.75rem;
+  font-weight: bold;
+  text-transform: uppercase;
+  color: rgba(46, 46, 46, 0.726);
+  @media (prefers-color-scheme: dark) {
+    color: rgba(255, 255, 255, 0.514);
+  }
+}
+
+.nav-link-icon {
+  font-size: 0.95rem;
+  margin-right: 0.5rem;
+  font-weight: bold;
+}
+.nav-link {
+  display: flex;
+  position: relative;
+  align-items: center;
+  color: #000000e1;
+  @media (prefers-color-scheme: dark) {
+    color: rgba(255, 255, 255, 0.795);
+  }
+}
+
+.text {
+  font-weight: 500;
+}
+</style>
